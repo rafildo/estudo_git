0ac0cfc186248a92686c6f197187c7f290db41a2
rafildo <raflb.cwb@gmail.com>
2023-11-21 00:10:28 -0300

    Nova Home finalizada

--- hoje.js
+++ hoje.js
@@ -1,3 +1,9 @@
 function test(){
+
+}
+
+function test2(){
     
 }
+
+//Finalizado sistema da home
--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 //trabalhando no login
 
 //finalizando sistema de login
+
+function login(nome, senha){
+    return "Bem Vindo";
+}
+
+//Seja bem vindo
+
+//Sistema de login completo
+//Retornamos uma mensagem de boas-vindas
