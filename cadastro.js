6343cdbf5b957ebc35f19fd266e8d0c35b33815b
rafildo <raflb.cwb@gmail.com>
2023-11-21 00:51:45 -0300

    Adicionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -3,3 +3,9 @@ function signUp(email){
 }
 
 //pegando o e-mail e cadastrando na plataforma
+
+function mostraMensagem(email){
+    return "Bem-vindo ao sistema";
+}
+
+//Adicionando função de mensagem
